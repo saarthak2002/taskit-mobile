ea8b67d0da3a8f4a8acb9d98c7a5a4bf08019da4
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-08-05 22:44:53 +0530

    finished profile view

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# taskit_mobile
+# task.it Mobile App
 
-A new Flutter project.
+This is a Flutter mobile app for the task.it platform. It allows users to modify their projects on the task.it platform easily on the go. Data is served to the task.it mobile app from the same [Flask backend](https://github.com/saarthak2002/taskit-backend) that is used by the [task.it web application](https://github.com/saarthak2002/taskit-app), ensuring that user data is always synced across different platforms.
+
+![task it mobile app cover](/screenshots/taskit-mobile-cover.png)
--- lib/screens/Profile.dart
+++ lib/screens/Profile.dart
@@ -3,6 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
+import 'package:taskit_mobile/extentions.dart';
+
 class Profile extends StatefulWidget {
   const Profile({ Key? key }) : super(key: key);
   @override
@@ -12,6 +14,8 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
 
   late Future userInfo = fecthUserInfo();
+  late Future userStats = fecthUserStats();
+  late Future<List<dynamic>> collabs = fetchAllCollabsForUser();
 
   @override
   Widget build(BuildContext context) {
@@ -21,16 +25,16 @@ class _ProfileState extends State<Profile> {
         centerTitle: true,
         backgroundColor: Colors.blue[900],
       ),
-      body: Column(
-        children: [
-          Container(
-            margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
-            padding: const EdgeInsets.only(bottom: 10),
-            child: FutureBuilder(
-              future: userInfo,
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if(snapshot.hasData) {
-                  return Row(
+      body: FutureBuilder(
+        future: userInfo,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if(snapshot.hasData) {
+            return Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
                     children: [
                       Expanded(
                         child: Text(
@@ -47,16 +51,199 @@ class _ProfileState extends State<Profile> {
                         child: const Text('Logout'),
                       ),
                     ],
-                  );
-                }
-                else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            )
-          ),
-        ],
-      ),
+                  ),
+                ),
+                CircleAvatar(
+                  radius: 80,
+                  backgroundColor: Colors.blue[900],
+                  child: Text(
+                    '${snapshot.data['firstname'].toString().capitalize().characters.first}${snapshot.data['lastname'].toString().capitalize().characters.first}',
+                    style: const TextStyle(
+                      fontSize: 50,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: Center(
+                    child: Text(
+                      '@${snapshot.data['username']}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w300,
+                        color: Color.fromARGB(255, 77, 76, 76),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 3),
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Center(
+                    child: Text(
+                      '${FirebaseAuth.instance.currentUser!.email}',
+                      style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w300,
+                        color: Color.fromARGB(255, 77, 76, 76),
+                      ),
+                    ),
+                  ),
+                ),
+                FutureBuilder(
+                  future: userStats,
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if(snapshot.hasData) {
+                      return Row(
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '${snapshot.data['total_projects']}',
+                                    style: const TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Projects',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w300,
+                                      color: Color.fromARGB(255, 77, 76, 76),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '${snapshot.data['total_tasks']}',
+                                    style: const TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Tasks',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w300,
+                                      color: Color.fromARGB(255, 77, 76, 76),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                              padding: const EdgeInsets.only(bottom: 10),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    '${snapshot.data['completed_tasks']}',
+                                    style: const TextStyle(
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w600,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  const Text(
+                                    'Completed',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w300,
+                                      color: Color.fromARGB(255, 77, 76, 76),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      );
+                    }
+                    else{
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                  }
+                ),
+                Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 20, top: 15),
+                      child: const Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Collaborations',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    FutureBuilder(
+                      future: collabs,
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if(snapshot.hasData) {
+                          return Container(
+                            margin: const EdgeInsets.only(left: 20, right: 20, top:10),
+                            height: 50,
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                var collab = snapshot.data[index];
+                                return Container(
+                                  padding: const EdgeInsets.all(5),
+                                  child: Tooltip(
+                                    message: '${collab['firstname']} ${collab['lastname']}',
+                                    child: CircleAvatar(
+                                      backgroundColor: Colors.blue[900],
+                                      child: Text('${collab['firstname'].toString().characters.first}${collab['lastname'].toString().characters.first}'),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          );
+                        }
+                        else {
+                          return Container(
+                            margin: const EdgeInsets.all(20),
+                            child: const Center(child: CircularProgressIndicator()),
+                          );
+                        }
+                      }
+                    ),
+                  ],
+                ),
+              ],
+            );
+          }
+          else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      )
     );
   }
 
@@ -66,5 +253,20 @@ class _ProfileState extends State<Profile> {
     var result = await http.get(Uri.parse("${BASE_API_URL}users/${user!.uid}"));
     var decodedResponse = jsonDecode(result.body);
     return decodedResponse;
+  }
+
+  Future fecthUserStats() async {
+    var user = FirebaseAuth.instance.currentUser;
+    const BASE_API_URL = String.fromEnvironment('BASE_API_URL', defaultValue: '');
+    var result = await http.get(Uri.parse("${BASE_API_URL}stats/basic/${user!.uid}"));
+    var decodedResponse = jsonDecode(result.body);
+    return decodedResponse;
+  }
+
+  Future<List<dynamic>> fetchAllCollabsForUser() async {
+    var user = FirebaseAuth.instance.currentUser;
+    const BASE_API_URL = String.fromEnvironment('BASE_API_URL', defaultValue: '');
+    var result = await http.get(Uri.parse("${BASE_API_URL}collabs/getcollabsbyuid/${user!.uid}"));
+    return jsonDecode(result.body);
   }
 }
